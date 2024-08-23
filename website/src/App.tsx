29ff70bd6636990a1e3a33ee6832438166867f38
Simeon Widdis <sawiddis@gmail.com>
2024-08-22 22:26:05 -0700

    Update frontend for new session API

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,22 +1,40 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [session, setSession] = useState("");
 
   useEffect(() => {
-    fetch('http://localhost:8081/api/get-count')
-      .then(response => response.json())
-      .then(data => setCount(data.count))
-  }, [])
+    fetch("http://localhost:8081/session", { method: "POST" })
+      .then((response) => response.json())
+      .then((data) => {
+        setCount(data.state.count);
+        setSession(data.session);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!session) {
+      return;
+    }
+    fetch(`http://localhost:8081/session/${session}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCount(data.state.count ?? 0);
+      });
+  }, [session]);
 
   const increment = () => {
-    fetch('http://localhost:8081/api/increment-count', { method: 'POST' })
-      .then(response => response.json())
-      .then(data => setCount(data.count))
-  }
+    fetch(`http://localhost:8081/session/${session}`, {
+      method: "POST",
+      body: JSON.stringify({ count: count + 1 }),
+    })
+      .then((response) => response.json())
+      .then((data) => setCount(data.state.count));
+  };
 
   return (
     <>
@@ -30,18 +48,22 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={increment}>
-          count is {count}
-        </button>
+        <button onClick={increment}>count is {count}</button>
+
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Using Session:{" "}
+          <input
+            type="text"
+            defaultValue={session}
+            onChange={(evt) => setSession(evt.target.value)}
+          />
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
