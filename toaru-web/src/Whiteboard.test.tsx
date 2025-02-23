dc9bce73f742434cf5ebb0ebd2e0bd2160fcea8c
Simeon Widdis <sawiddis@gmail.com>
2025-02-23 11:46:53 -0800

    Use the line method in tests, dingus

--- toaru-web/src/Whiteboard.test.tsx
+++ toaru-web/src/Whiteboard.test.tsx
@@ -1,21 +1,12 @@
 import { reducer } from "./Whiteboard";
-import { Point, Shape } from "./geometry";
-
-const testLine = (id: string, points: Point[]): Shape => ({
-  id,
-  type: "line",
-  points,
-});
+import { line, Shape } from "./geometry";
 
 describe("reducer", () => {
   test("creates a shape", () => {
     const initialState: Shape[] = [];
     const action = {
       type: "create" as const,
-      shape: testLine("1", [
-        { x: 0, y: 0 },
-        { x: 10, y: 10 },
-      ]),
+      shape: line({ x: 0, y: 0 }, { x: 10, y: 10 }),
     };
     const newState = reducer(initialState, action);
 
@@ -24,17 +15,12 @@ describe("reducer", () => {
   });
 
   test("updates a shape's points", () => {
-    const initialState: Shape[] = [
-      testLine("1", [
-        { x: 0, y: 0 },
-        { x: 10, y: 10 },
-      ]),
-    ];
+    const initialState: Shape[] = [line({ x: 0, y: 0 }, { x: 10, y: 10 })];
     const updatedPoints = [
       { x: 5, y: 5 },
       { x: 15, y: 15 },
     ];
-    const action = { type: "update" as const, id: "1", points: updatedPoints };
+    const action = { type: "update" as const, id: initialState[0].id, points: updatedPoints };
     const newState = reducer(initialState, action);
 
     expect(newState).toHaveLength(1);
@@ -42,28 +28,18 @@ describe("reducer", () => {
   });
 
   test("deletes a shape", () => {
-    const initialState: Shape[] = [
-      testLine("1", [
-        { x: 0, y: 0 },
-        { x: 10, y: 10 },
-      ]),
-    ];
-    const action = { type: "delete" as const, id: "1" };
+    const initialState: Shape[] = [line({ x: 0, y: 0 }, { x: 10, y: 10 })];
+    const action = { type: "delete" as const, id: initialState[0].id };
     const newState = reducer(initialState, action);
 
     expect(newState).toHaveLength(0);
   });
 
   test("ignores updates to non-existent shapes", () => {
-    const initialState: Shape[] = [
-      testLine("1", [
-        { x: 0, y: 0 },
-        { x: 10, y: 10 },
-      ]),
-    ];
+    const initialState: Shape[] = [line({ x: 0, y: 0 }, { x: 10, y: 10 })];
     const action = {
       type: "update" as const,
-      id: "2",
+      id: "nonexistent",
       points: [
         { x: 5, y: 5 },
         { x: 15, y: 15 },
