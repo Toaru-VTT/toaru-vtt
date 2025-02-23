0ef32341b3886e333737398f77d27bc6779b2b3f
Simeon Widdis <sawiddis@gmail.com>
2025-02-22 21:20:46 -0800

    Fix the web build

--- toaru-web/src/App.test.tsx
+++ toaru-web/src/App.test.tsx
@@ -1,8 +1,9 @@
-import { render, h } from "preact";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  render(<App />, div);
-  render(null, div);
+  const container = document.createElement("div");
+  const root = createRoot(container);
+  root.render(<App />);
+  root.render(null);
 });
